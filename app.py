1b8e9014f66113f45c39a9f1d7578a3e1119606d
anotherOne <decemberintelijidea@gmail.com>
2024-12-16 20:36:49 +0300

    recaptcha added

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import requests
 
 app = Flask(__name__)
 
+# reCAPTCHA settings
+RECAPTCHA_SITE_KEY = '6LdQiJ0qAAAAALcrQ7rGVb7WhtZSwCcNa8u_2QTw'
+RECAPTCHA_SECRET_KEY = '6LdQiJ0qAAAAAPc9JXFnOsB-o4GzoFh0jYIG1PO3'
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template("root.html")
+def index():
+    return render_template('captcha.html', recaptcha_site_key=RECAPTCHA_SITE_KEY)
 
+@app.route('/verify', methods=['POST'])
+def verify():
+    data = {
+        'secret': RECAPTCHA_SECRET_KEY,
+        'response': request.form['g-recaptcha-response']
+    }
+    response = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
+    result = response.json()
+
+    if result['success']:
+        return redirect('/rotate')
+    else:
+        return "reCAPTCHA verification failed, please try again."
+
+@app.route('/rotate')
+def hello_world():
+    return render_template("root.html")
 
 if __name__ == '__main__':
     app.run()
